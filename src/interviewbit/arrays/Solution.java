8f4720921ab7b92d08001fa11bd3bb569b3e7b72
Piyush <piyush035@gmail.com>
2019-08-13 15:50:49 +0530

    Add for Arrays pratice

---
+++ src/interviewbit/arrays/Solution.java
@@ -0,0 +1,16 @@
+package interviewbit.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+    }
+
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public ArrayList<Integer> spiralOrder(final List<ArrayList<Integer>> A) {
+
+    }
+}
