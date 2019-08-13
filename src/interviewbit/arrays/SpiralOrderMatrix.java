24b43bfefd7dcfa236cdffc1afe835d8fdc3be0a
Piyush Gupta <piyush035@gmail.com>
2019-08-13 21:09:32 +0530

    Added for arrays pratice

--- src/interviewbit/arrays/SpiralOrderMatrix.java
+++ src/interviewbit/arrays/SpiralOrderMatrix.java
@@ -3,7 +3,7 @@ package interviewbit.arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
+public class SpiralOrderMatrix {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
 
