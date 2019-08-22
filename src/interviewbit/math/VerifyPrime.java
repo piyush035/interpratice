1c2c275a86ae28be2c232a42b51d558b80ec8b8e
Piyush <piyush035@gmail.com>
2019-08-22 13:12:08 +0530

    Add for Math pratice

---
+++ src/interviewbit/math/VerifyPrime.java
@@ -0,0 +1,17 @@
+package interviewbit.math;
+
+public class VerifyPrime {
+    public int isPrime(int A) {
+        if(A <= 1){
+            return 0;
+        }
+        for(int i=2;i<=Math.sqrt(A);i++){
+            if(A%i==0)
+                return 0;
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        System.out.println("Result ::" + new VerifyPrime().isPrime(7));
+    }
+}
