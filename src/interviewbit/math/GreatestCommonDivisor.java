f7a62a53963799f27cacaaa77795cdd4d795dafa
Piyush <piyush035@gmail.com>
2019-08-29 11:47:58 +0530

    Add for Math pratice

---
+++ src/interviewbit/math/GreatestCommonDivisor.java
@@ -0,0 +1,26 @@
+package interviewbit.math;
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        System.out.println("Result ::" + new GreatestCommonDivisor().gcd(1000000000, 999990090));
+    }
+
+    public int gcd(int A, int B) {
+        int result = 0;
+        if (A < 0 || B < 0)
+            return result;
+        if (A == 0 && B != 0)
+            return B;
+        if (B == 0 && A != 0)
+            return A;
+        if (B == A)
+            return A;
+        int min = A < B ? A : B;
+        for (int i = 1; i <= min ; i++) {
+            if ((A % i == 0 && B % i == 0) && i > result) {
+                result = i;
+            }
+        }
+        return result;
+    }
+}
