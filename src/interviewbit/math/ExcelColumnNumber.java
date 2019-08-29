9cacbcd32235f702a63d70b8b9393373b55d4ccc
Piyush <piyush035@gmail.com>
2019-08-29 11:01:13 +0530

    Add for Math pratice

--- src/interviewbit/math/ExcelColumnNumber.java
+++ src/interviewbit/math/ExcelColumnNumber.java
@@ -1,14 +1,21 @@
 package interviewbit.math;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExcelColumnNumber {
     public static void main(String[] args) {
         System.out.println("Result ::" + new ExcelColumnNumber().titleToNumber("ABC"));
     }
     public int titleToNumber(String A) {
         int result = 0;
+        List<String> list = new ArrayList<String>();
+        list.add("A");list.add("B");list.add("C");list.add("D");list.add("E");list.add("F");list.add("G");list.add("H");list.add("I");list.add("J");list.add("K");
+        list.add("L");list.add("M");list.add("N");list.add("O");list.add("P");list.add("Q");list.add("R");list.add("S");list.add("T");list.add("U");list.add("V");
+        list.add("W");list.add("X");list.add("Y");list.add("Z");
         char[] chars = A.toCharArray();
         for (int i=0; i<chars.length;i++){
-            result = result*26 + (chars[i]-'A'+1);
+            result += result*25 + list.indexOf(""+chars[i])+1;
         }
         return result;
     }
--- src/interviewbit/math/ExcelColumnTitle.java
+++ src/interviewbit/math/ExcelColumnTitle.java
@@ -2,13 +2,20 @@ package interviewbit.math;
 
 public class ExcelColumnTitle {
     public static void main(String[] args) {
-        System.out.println("Result ::" + new ExcelColumnTitle().convertToTitle(1));
+        System.out.println("Result ::" + new ExcelColumnTitle().convertToTitle(980089));
     }
     public String convertToTitle(int A) {
         StringBuilder sb = new StringBuilder();
         while(A > 0){
-
+            int rem = A%26;
+            if(rem == 0){
+                sb.append("Z");
+                A = (A/26)-1;
+            }else{
+                sb.append((char)((rem-1)+'A'));
+                A = A/26;
+            }
         }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
