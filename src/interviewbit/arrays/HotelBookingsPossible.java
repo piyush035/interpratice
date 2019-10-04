93b5d177d78bea593b3bd8be88f71cf56d75edbc
Piyush Gupta <piyush035@gmail.com>
2019-10-04 20:39:52 +0530

    Added for arrays pratice

---
+++ src/interviewbit/arrays/HotelBookingsPossible.java
@@ -0,0 +1,21 @@
+package interviewbit.arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class HotelBookingsPossible {
+    public static void main(String[] args) {
+
+        System.out.println("Result ::" + new HotelBookingsPossible().hotel(null,null,0));
+    }
+    public boolean hotel(ArrayList<Integer> arrive, ArrayList<Integer> depart, int K) {
+        Collections.sort(arrive);
+        Collections.sort(depart);
+        for(int i=0; i<arrive.size();i++){
+            if(i+K < arrive.size() && arrive.get(i+K)<depart.get(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
