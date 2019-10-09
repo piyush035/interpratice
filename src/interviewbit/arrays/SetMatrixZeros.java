650830d0591eb45c61d3bc88754f3a03ae3f16ed
Piyush <piyush035@gmail.com>
2019-10-09 14:35:20 +0530

    Add for Math pratice

---
+++ src/interviewbit/arrays/SetMatrixZeros.java
@@ -0,0 +1,11 @@
+package interviewbit.arrays;
+
+import java.util.ArrayList;
+
+public class SetMatrixZeros {
+    public static void main(String[] args) {
+
+    }
+    public void setZeroes(ArrayList<ArrayList<Integer>> a) {
+    }
+}
