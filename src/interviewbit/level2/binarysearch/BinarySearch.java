785f4f34f53929d7ff7b3347d51c5822bb3d2e73
Piyush <piyush035@gmail.com>
2019-10-15 18:05:12 +0530

    Add for binary search

---
+++ src/interviewbit/level2/binarysearch/BinarySearch.java
@@ -0,0 +1,29 @@
+package interviewbit.level2.binarysearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,5,7,11,13,15,17,19};
+        System.out.println("Result :: "+binarySearch(arr,3));
+    }
+    /**
+     * Return the index of element if available otherwise -1
+     *
+     * @param arr
+     * @param target
+     * @return
+     */
+    public static int binarySearch(int arr[], int target) {
+        int start = 0, end = arr.length - 1, mid = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
