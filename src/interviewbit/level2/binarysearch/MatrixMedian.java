d52c7769ac917f612eecebfe83b90395cffd442a
Piyush <piyush035@gmail.com>
2019-10-19 13:46:33 +0530

    Add for binary search

--- src/interviewbit/level2/binarysearch/MatrixMedian.java
+++ src/interviewbit/level2/binarysearch/MatrixMedian.java
@@ -22,10 +22,11 @@ public class MatrixMedian {
         System.out.println("Matrix :: " + new MatrixMedian().findMedian(list));
     }
 
-    public int findMedian1(ArrayList<ArrayList<Integer>> A) {
+    //Used an extra array and sorting
+    public int findMedian1(List<List<Integer>> A) {
         int median = 0;
         List<Integer> list = new ArrayList<>();
-        for (ArrayList<Integer> arrList : A) {
+        for (List<Integer> arrList : A) {
             for (int i : arrList) {
                 list.add(i);
             }
@@ -36,46 +37,40 @@ public class MatrixMedian {
         return median;
     }
 
+    //Without extra array
     public int findMedian(List<List<Integer>> A) {
         /*int min = Integer.MIN_VALUE;
         int max = Integer.MAX_VALUE;*/
-        int min = findMin(A), max = findMax(A), medianSize = A.size() * A.get(0).size()/2;
-
-        while(min <=max){
-            int mid = (min+max)/2;
-            int countSmall = countSmallInMatrix(A,mid);
+        int min = findMin(A), max = findMax(A), medianCount = A.size() * A.get(0).size() / 2;
+        while (min <= max) {
+            int mid = (int) (min + ((long) max - min) / 2);;
+            int countSmall = countSmallInMatrix(A, mid);
+            if (countSmall > medianCount) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
         }
-        /*while (left <= right) {
-            int mid = (int) (left + ((long) right - left) / 2);
-            int smaller = countSmaller1(A, mid);
-            if (smaller > total / 2)
-                right = mid - 1;
-            else
-                left = mid + 1;
-        }*/
-        return 0;//right;
+        return max;
     }
 
     private int countSmallInMatrix(List<List<Integer>> A, int B) {
         int count = 0;
-        for(int i=0;i<A.size();i++){
-
-        }
-        /*int smaller = 0;
         for (int i = 0; i < A.size(); i++) {
-            smaller += countSmaller(A.get(i), B);
+            count += countSmall(A.get(i), B);
         }
-        return smaller;*/
+        return count;
     }
 
-    private int countSmaller(final List<Integer> A, int B) {
+    private int countSmall(final List<Integer> A, int B) {
         int left = 0, right = A.size() - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (B <= A.get(mid))
+            if (A.get(mid) >= B) {
                 right = mid - 1;
-            else // if B > A.get(mid)
+            } else {
                 left = mid + 1;
+            }
         }
         return left;
     }
@@ -83,7 +78,7 @@ public class MatrixMedian {
     private int findMin(List<List<Integer>> list) {
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < list.size(); i++) {
-            int value  = list.get(i).get(0);
+            int value = list.get(i).get(0);
             if (min > value) {
                 min = value;
             }
@@ -94,7 +89,7 @@ public class MatrixMedian {
     private int findMax(List<List<Integer>> list) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < list.size(); i++) {
-            int value = list.get(i).get(list.get(i).size()-1);
+            int value = list.get(i).get(list.get(i).size() - 1);
             if (max < value) {
                 max = value;
             }
