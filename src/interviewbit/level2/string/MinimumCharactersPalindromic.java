cbf23f27c7877e2e0b83bf998054cb35e013b06a
Piyush Gupta <piyush035@gmail.com>
2019-10-19 19:52:05 +0530

    Added for binary Search

---
+++ src/interviewbit/level2/string/MinimumCharactersPalindromic.java
@@ -0,0 +1,28 @@
+package interviewbit.level2.string;
+
+public class MinimumCharactersPalindromic {
+    public static void main(String[] args) {
+        System.out.println("MinimumCharactersPalindromic :: " + new MinimumCharactersPalindromic().solve("ABC"));
+    }
+
+    public int solve(String A) {
+        int end = A.length() - 1, count = 0;
+        for (int i = 0; i <= end; i++) {
+            if (!isPalindrome(A, 0, end - count)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    boolean isPalindrome(String A, int start, int end) {
+        while (start <= end) {
+            if (A.charAt(start) != A.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
